ca45146ab6863cfd46dc1ce3d640ead60aae3354
Iacopo454 <iacopo.gentile@icloud.com>
2021-08-01 11:40:21 +0000

    load the fixtures for the json products and categories, and done migrations, now my products display on the admin of the app

--- products/admin.py
+++ products/admin.py
@@ -1,34 +1,6 @@
-boutique_ado_v1/products/admin.py /
-@ckz8780
-ckz8780 Customized admin
-Latest commit 1033bd2 on 31 Jan 2020
- History
- 1 contributor
-25 lines (20 sloc)  492 Bytes
-  
 from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
-
-    ordering = ('sku',)
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
-
+admin.site.register(Product)
+admin.site.register(Category)
