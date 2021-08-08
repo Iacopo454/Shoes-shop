5f148658fd55179df65cab38ec4798ac14aa2943
Iacopo454 <iacopo.gentile@icloud.com>
2021-08-08 15:29:13 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key':'pk_test_51JMDCRCrm7XTEgfUKAhGyunofmu0CyXR6KBOX2DjXB82eJQDb5cHD1BzKFXW8lQ0OG9hOxInqPMUnfWBoYKNXYmT00SEmMMShG',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
