9eb99295a560b68d936a3713134e87a5d1d3b98c
Iacopo454 <iacopo.gentile@icloud.com>
2021-08-13 21:07:12 +0000

    card payment field not working

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
+    check = settings.STRIPE_SECRET_KEY
+    print(check)
+    print("testing")
 
     return render(request, 'home/index.html')
