90ff7f79151bf59bfc88664b8df743d026535ca2
Iacopo454 <iacopo.gentile@icloud.com>
2021-10-24 11:28:19 +0000

    fixtures reload as had different errors, and enviroment variables set correctly in the env.py

--- wishlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.wishlist, name="wishlist"),
-    path("add/<item_id>/", views.add_to_wishlist, name="add_to_wishlist"),
-]
